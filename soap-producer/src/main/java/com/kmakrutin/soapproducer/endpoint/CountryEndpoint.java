a36b66046dba8b561518813415c8257cd15d1856
Kirill Makrutin <k.makrutin@gmail.com>
2021-05-22 16:03:53 +0300

    Fixed SOAP message producer

--- soap-producer/src/main/java/com/kmakrutin/soapproducer/endpoint/CountryEndpoint.java
+++ soap-producer/src/main/java/com/kmakrutin/soapproducer/endpoint/CountryEndpoint.java
@@ -3,13 +3,20 @@ package com.kmakrutin.soapproducer.endpoint;
 import com.kmakrutin.soap.producer.GetCountryRequest;
 import com.kmakrutin.soap.producer.GetCountryResponse;
 import com.kmakrutin.soapproducer.repo.CountryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import java.util.Optional;
 
 @Endpoint
 public class CountryEndpoint {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryEndpoint.class);
+
     public static final String NAMESPACE_URI = "http://kmakrutin.com/soap/producer";
 
     private final CountryRepository countryRepository;
@@ -20,7 +27,10 @@ public class CountryEndpoint {
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
     public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        LOGGER.info("Get country request: {}", Optional.ofNullable(request).map(GetCountryRequest::getName).orElse(null));
+
         GetCountryResponse response = new GetCountryResponse();
         response.setCountry(countryRepository.findCountry(request.getName()));
 
